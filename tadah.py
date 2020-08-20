5f339d04ea750b72e80f51f4595ab2524742c2ef
Arianna Masciolini <uzkamascio@gmail.com>
2020-08-20 17:12:01 +0200

    polished pdf part

--- tadah.py
+++ tadah.py
@@ -3,7 +3,11 @@ from os.path import splitext
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PyPDF2.generic import NameObject, createStringObject
 
-def editPDF(path, title, author):
+def editPDFinfo(path, title, author):
+  '''
+  given the path to a pdf, a new title and a new author,
+  it updates the pdf's title and author InfoKeys
+  '''
   with open(path,mode="rb") as file:
     info = (PdfFileReader(file)).getDocumentInfo()
   info.update({
@@ -11,13 +15,13 @@ def editPDF(path, title, author):
     NameObject('/Title'): createStringObject(title),
     NameObject('/Author'): createStringObject(author)
   })
-
   writer = PdfFileWriter()
   writer.addMetadata(info)
   with open(path,mode="wb") as file:
     writer.write(file)
 
 if __name__ == '__main__':
+  # manage command line args
   parser = argparse.ArgumentParser(description='Edit ebook metadata.')
   parser.add_argument('book', help='path to an EPUB or PDF FILE')
   parser.add_argument('title', help='new title')
@@ -27,10 +31,12 @@ if __name__ == '__main__':
   book_path = args.book
   title = args.title
   author = args.author
-
   (filename, ext) = splitext(book_path)
-  if ext.lower() == '.pdf':
-    editPDF(book_path, title, author)
+  ext = ext.lower()
+
+  # handle the various formats 
+  if ext == '.pdf':
+    editPDFinfo(book_path, title, author)
   else: 
     print('file format not supported (yet)!')
     exit(0)
