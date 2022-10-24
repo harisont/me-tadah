5472e53e6f706e33aec7f25afbc4c60234e0e55a
Arianna Masciolini <uzkamascio@gmail.com>
2022-10-24 22:28:29 +0200

    add audio files support

--- README.md
+++ README.md
@@ -1,28 +1,49 @@
 # Me-tadah!
 
-There are two types of metadata editors: those that don't work, and those that are just a tiny tiny part of a gigantic ebook management programme.
+There are two types of metadata editors: those that don't work, and those that are just a tiny tiny part of a gigantic ebook/music management programme.
+
+Me-tadah, on the contrary, allows you to quickly edit title author/artist of your ebooks and audio files, and that's it.
 
 ## Usage
-If you just want to be able to modify the title and the author of your ebooks, you can just
+If all you wanted was editing the title and the author/artist of your ebooks or audio files, you just open a terminal and type
 
 ```
-tadah path_to_ebook new_title new_author
+tadah path_to_file new_title new_author/artist
 ```
+
+... or something like that.
+
+If you also care about album name, artwork, publisher, cover image, genre, year and so on, you are welcome to open a pull request. With the libraries I'm using, everything should be simple, but I'm too lazy to implement all these things and honestly, 99% of the times I don't really care about anything else.
 
 ## Supported formats
+
+### Ebooks
 - `.epub`
 - `.pdf`
+
+### Audio
+- `.aac` 
+- `.aiff` 
+- `.dsf` 
+- `.flac` 
+- `.m4a` 
+- `.mp3` 
+- `.ogg` 
+- `.opus` 
+- `.wav` 
+- `.w`
 
 ## Dependencies
 
 - `pdfrw`
 - `ebooklib`
+- `music_tag`
 
 ## Installation
 Just like any other Python program (assuming you have Python installed):
 
 1. install the dependencies, for example with `pip install [dep]`
-2. clone this reporitory
+2. clone this repository
 3. move inside it
 4. mark the Python file as executable (`chmod +x`)
 5. add, as the file's first line, the path to your Python interpreter (for example `#!/usr/bin/env python`)
--- tadah.py
+++ tadah.py
@@ -1,12 +1,26 @@
-import argparse
+from argparse import ArgumentParser
 from os.path import splitext, isfile
 from pdfrw import PdfReader, PdfWriter
 from ebooklib.epub import read_epub, write_epub
+from music_tag import load_file
+
+MUSIC_FILE_FORMATS = [
+  '.aac', 
+  '.aiff', 
+  '.dsf', 
+  '.flac', 
+  '.m4a', 
+  '.mp3', 
+  '.ogg', 
+  '.opus', 
+  '.wav', 
+  '.wv'
+  ]
 
 def editPDFinfo(path, title, author):
   '''
-  given the path to a pdf, a new title and a new author,
-  it updates the pdf's title and author InfoKeys
+  given the path to a .pdf file, a new title and a new author,
+  it updates the ebook's title and author InfoKeys
   '''
   pdf = (PdfReader(path))
   pdf.Info.Title = title
@@ -15,8 +29,8 @@ def editPDFinfo(path, title, author):
 
 def editEPUBinfo(path, title, author):
   '''
-  given the path to an epub, a new title and a new author,
-  it updates the pdf's title and author InfoKeys
+  given the path to an .epub file, a new title and a new author,
+  it updates the ebook's title and creator
   '''
   epub = read_epub(path)
   # not using set_title and add_author cause they don't update existing fields
@@ -24,29 +38,40 @@ def editEPUBinfo(path, title, author):
   epub.set_unique_metadata('DC', 'creator', author) # creator = author!?
   write_epub(path, epub)
 
+def editAUDIOinfo(path, title, author):
+  '''
+  given the path to an audio file, a new title and a new author,
+  it updates the audio file's tracktitle and artist field
+  '''
+  audio = load_file(path)
+  audio['tracktitle'] = title
+  audio['artist'] = author
+
 
 if __name__ == '__main__':
   # manage command line args
-  parser = argparse.ArgumentParser(description='Edit ebook metadata.')
-  parser.add_argument('book', help='path to an EPUB or PDF FILE')
+  parser = ArgumentParser(description='Edit ebook and audio metadata.')
+  parser.add_argument('file', help='path to an EPUB, PDF or audio file')
   parser.add_argument('title', help='new title')
-  parser.add_argument('author', help='new author')
+  parser.add_argument('author', help='new author/artist')
   args = parser.parse_args()
 
-  book_path = args.book
+  file_path = args.file
   title = args.title
   author = args.author
-  (filename, ext) = splitext(book_path)
+  (filename, ext) = splitext(file_path)
   ext = ext.lower()
 
-  if not isfile(book_path):
+  if not isfile(file_path):
     print('that\'s not a valid path, you liar!')
     exit(1)
   # handle the various formats 
   if ext == '.pdf':
-    editPDFinfo(book_path, title, author)
+    editPDFinfo(file_path, title, author)
   elif ext == '.epub':
-    editEPUBinfo(book_path, title, author)
+    editEPUBinfo(file_path, title, author)
+  elif ext in MUSIC_FILE_FORMATS:
+    editAUDIOinfo(file_path, title, author)
   else: 
     print('file format not supported (yet)!')
     exit(0)
